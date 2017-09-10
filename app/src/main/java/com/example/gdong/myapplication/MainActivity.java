d6053e799d581c3080963b09d76571c596b66512
gdong <897309731@qq.com>
2017-09-10 21:50:33 +0800

    完善ui界面

--- app/src/main/java/com/example/gdong/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gdong/myapplication/MainActivity.java
@@ -3,22 +3,34 @@ package com.example.gdong.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button= (Button) findViewById(R.id.main_select);
+        editText = (EditText) findViewById(R.id.main_editText);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent= new Intent(MainActivity.this,CusDetailActivity.class);
-                startActivity(intent);
+                if (editText.getText().toString()!=null&&editText.getText().toString().equals("123456789") ){
+                    Intent intent= new Intent(MainActivity.this,ResultActivity.class);
+                    startActivity(intent);
+                }
+                else {
+                    Intent intent= new Intent(MainActivity.this,CusDetailActivity.class);
+                    startActivity(intent);
+
+                }
 
             }
         });
